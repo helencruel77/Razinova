dc16e1f4a16282324c650a66cf81a38c4ed81132
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-23 23:13:51 +0300

    add method

--- OS2/src/Planner.java
+++ OS2/src/Planner.java
@@ -1,11 +1,25 @@
+import java.util.List;
 
 public class Planner {
 
 	private int Count = 0;
+	private List<Process> processes;
+	int random = (int) Math.random() * 5;
+	
 	public Planner(int Count) {
-		
+		for(int i = 0; i < Count; i++) {
+			processes.add(new Process(i, random));
+		}
 	}
 	
+	public void run() {
+		while(!processes.isEmpty()) {
+			for (int i = 0; i < processes.size(); i++) {
+				System.out.println("Выполняется" + processes.get(i).getId());
+				processes.get(i).run();
+			}
+		}
+	}
 	
 	
 }
