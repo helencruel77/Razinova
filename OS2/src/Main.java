bb49d586db741f1c2227ba45400f0c45bfcc47b6
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-23 22:39:03 +0300

    change

--- OS2/src/Main.java
+++ OS2/src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		
+		int random = (int) Math.random() * 5;
+		Planner planner = new Planner(random);
 	}
 }
