705369ff7669c635290ac25525c1084a7da892dc
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-23 23:48:17 +0300

    fix

--- OS2/src/Planner.java
+++ OS2/src/Planner.java
@@ -4,16 +4,17 @@ public class Planner {
 
 	private int Count = 0;
 	private List<Process> processes;
-	int random = (int) Math.random() * 5;
+	int randomTime = (int) Math.random() * 7;
+	int randomCount = (int) Math.random() * 5;
 	
 	public Planner(int Count) {
 		for(int i = 0; i < Count; i++) {
-			processes.add(new Process(i, random));
+			processes.add(new Process(i, randomTime, randomCount));
 		}
 	}
 	
 	public void run() {
-		while(!processes.isEmpty()) {
+		while(processes.size() != 0) {
 			for (int i = 0; i < processes.size(); i++) {
 				System.out.println("Выполняется" + processes.get(i).getId());
 				processes.get(i).run();
