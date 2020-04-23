79f1701c5addee9085c628b811bd20ccebf77cb7
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-23 21:54:17 +0300

    add Process

---
+++ OS2/src/Process.java
@@ -0,0 +1,11 @@
+
+public class Process {
+
+	private int id = 0;
+	private int quant_Process = 0;
+	
+	public Process (int id, int quant_Process) {
+		this.id = id;
+		this.quant_Process = quant_Process;
+	}
+}
