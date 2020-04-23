c6ac7aa93fe0e0a846d1b0018d7aa6c8b2e8bd56
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-24 01:55:22 +0300

    change method

--- OS2/src/Planner.java
+++ OS2/src/Planner.java
@@ -11,16 +11,29 @@ public class Planner {
 	public Planner(int Count) {
 		this.Count = Count;
 		processes = new ArrayList<Process>();
-		for(int i = 1; i < Count; i++) {
+		for(int i = 0; i < Count; i++) {
 			processes.add(new Process(i, randomTime.nextInt(10)+5, randomCount.nextInt(5) + 1));
 		}
 	}
 	
 	public void run() {
-		while(processes.size() != 0) {
+		int flag = 0;
+		while(processes.size() >= 0) {
 			for (int i = 0; i < processes.size(); i++) {
-				System.out.println("Выполняется" + processes.get(i).getId());
-				processes.get(i).run();
+				System.out.println("Выполняется " + processes.get(i).getId());
+				while (processes.get(i).getTime() > 0) {
+					processes.get(i).run();
+					if(processes.get(i).Size() - flag == 0) {
+						System.out.println(processes.get(i).getId() + " завершен");
+						processes.remove(i);
+						i--;
+						break;
+					}
+					if (processes.get(i).restore()) {
+						break;
+					}
+					flag++;
+				}
 			}
 		}
 	}
