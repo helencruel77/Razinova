9a3da8a3886ff9053f3beabf6d76c0c2ae4294cb
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-24 13:56:12 +0300

    fix and change

--- OS2/src/Thread.java
+++ OS2/src/Thread.java
@@ -4,12 +4,10 @@ public class Thread {
 	private int id = 0;
 	private int quant_Thread = 0;
 	private int work;
-	private int Count = 0;
 	
-	public Thread (int id, int quant_Thread, int Count) {
+	public Thread (int id, int quant_Thread) {
 		this.id = id;
 		this.quant_Thread = quant_Thread;
-		this.Count = Count;
 	}
 	
 	public int getQuant() {
