8615a575cc9ada85f0113b51b76271f368024fdd
helencruel228@outlook.com <helencruel228@outlook.com>
2020-05-15 08:34:45 +0300

    add methods

--- OS3/src/Page.java
+++ OS3/src/Page.java
@@ -3,9 +3,14 @@ public class Page {
 
 	private boolean availability;
 	private boolean R;
+	private int indexRealPage;
 	
 	public Page (boolean availability) {
 		this.availability = availability;
+	}
+	
+	public int getIndexRealPage() {
+		return indexRealPage;
 	}
 	
 	public boolean getAvailability() {
