16faf034402f59e6803d0d9d6a2592c70d6fb2df
helencruel228@outlook.com <helencruel228@outlook.com>
2020-05-15 08:17:58 +0300

    add

--- OS3/src/Page.java
+++ OS3/src/Page.java
@@ -2,8 +2,18 @@
 public class Page {
 
 	private boolean availability;
+	private boolean R;
 	
 	public Page (boolean availability) {
 		this.availability = availability;
 	}
+	
+	public void setR(boolean R) {
+		this.R = R;
+	}
+	
+	public boolean getR() {
+		return R;
+	}
+	
 }
